3aebd7568b2e86cdc4e7604ed5b2985600718679
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-11 18:12:00 +0530

    print data

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,18 +1,15 @@
 import axios from "axios";
 import { resolve } from "path";
 
-const wait = (ms: number) => new Promise((resolve)=> setTimeout(resolve, ms))
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const DashboardPage = async () => {
-
-  await wait(5)
+  await wait(5);
   const { data } = await axios.get(
     "https://jsonplaceholder.typicode.com/posts/1",
   );
 
-  console.log({ data });
-
-  return <div>Dashboard</div>;
+  return <div>{JSON.stringify(data)}</div>;
 };
 
 export default DashboardPage;
