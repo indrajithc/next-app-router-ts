b21d7c9f77cb6ceebdcaca06a902f0f4a8a73530
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-11 21:57:27 +0530

    custom error added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { AuthRequiredError } from "./lib/exceptions";
+
 const session = null;
 
 
 export default function Home() {
 
-  if(!session ) throw new Error("Auth is required to access this resource")
+  if(!session ) throw new AuthRequiredError("");
 
   return (<div>This is an auth-only page</div>)
 }
