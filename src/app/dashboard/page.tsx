016c414299a04ade09851e45d2a51303c40a8a59
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-12 12:07:49 +0530

    cache

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,22 @@
 import { FC } from "react";
 
-interface DashboardPageProps {
- 
-}
+interface DashboardPageProps {}
 
-const PageProps  = async(props: DashboardPageProps) => {
-  return <div>Dashboard</div>;
+const PageProps = async (props: DashboardPageProps) => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    cache: "no-store",
+  });
+
+  const data = await res.json();
+
+  console.log({ data });
+
+  return (
+    <div>
+      <div>Dashboard</div>
+      <div>{JSON.stringify(data)}</div>
+    </div>
+  );
 };
 
 export default PageProps;
