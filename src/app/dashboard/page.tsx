8812fb8439082ec10909a67cb880daf29b05f9ab
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-12 11:34:16 +0530

    dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+interface DashboardPageProps {
+ 
+}
+
+const PageProps  = async(props: DashboardPageProps) => {
+  return <div>Dashboard</div>;
+};
+
+export default PageProps;
