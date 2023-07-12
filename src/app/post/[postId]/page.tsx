de9172b4966eaa781814bb4f9303bf1dbbbfbed1
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-12 13:53:36 +0530

    meta data for page

--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -1,20 +1,26 @@
-import { FC } from "react";
-
-export async function generateStaticParams() {
-  const posts= ["post-one", "post-two"];
-  
-  return posts.map((post) => {
-
-    return ({
-      postId: post
-    })
-  })
-}
+import { Metadata } from "next";
 
 interface PostPageProps {
   params: {
     postId: string;
   };
+}
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+
+export async function generateMetadata ({ params}: PostPageProps) : Promise<MetaData> {
+
+  const res = await fetch( `https://jsonplaceholder.typicode.com/posts/${params.postId}`);
+  const data = await res.json() as Post;
+  return {
+    title: data.title
+  }
 }
 
 const PageProps  = async(props: PostPageProps) => {
