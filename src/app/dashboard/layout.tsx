36c2af5dcb5a3be0af3bdd340b26292a28612e8f
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-12 13:26:27 +0530

    layouts

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,12 @@
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      dashboard layout
+      {children}
+    </div>
+  );
+}
