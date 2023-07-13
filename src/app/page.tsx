7abcdc01fd838938fb02d5cf32228dc180d42331
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-13 15:29:58 +0530

    hero imported

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import Hero from "@components/Hero";
 
 export default function Home() {
-  return <main className="overflow-hidden">hi</main>;
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+    </main>
+  );
 }
