9ae07c47f1311eb94025efeb4fdd569bc596dcb5
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-17 17:44:42 +0530

    cars mapped

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 import Hero from "@components/Hero";
 import SearchBar from "@components/SearchBar";
 import CustomFilter from "@components/CustomFilter";
+import CarCard from "@components/CarCard";
 
 import { fetchCars } from "@/utils";
 
 export default async function Home() {
   const allCars = await fetchCars({});
 
-  console.log({ allCars });
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
     <main className="overflow-hidden">
@@ -25,6 +26,20 @@ export default async function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car, index) => (
+                <CarCard car={car} key={`ey${index + 1}`} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no result</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
