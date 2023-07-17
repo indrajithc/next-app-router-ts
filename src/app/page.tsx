fe19c58f01a8acbc0614eeabf655371f0f736185
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-17 12:59:08 +0530

    components mapped

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
 import Image from "next/image";
 import Hero from "@components/Hero";
+import SearchBar from "@components/SearchBar";
+import CustomFilter from "@components/CustomFilter";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4x1 font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
