c884d0e9c6a6b1a699224706c9efd18baf19d409
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-17 12:44:35 +0530

    import fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 import Navbar from "@components/Navbar";
-import Footer from "@components/Navbar";
+import Footer from "@components/Footer";
 
 export const metadata: Metadata = {
   title: "Car Hub",
