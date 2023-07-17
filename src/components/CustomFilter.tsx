d0ffc0a6be9cd75428c6bfcee40beca6769d448d
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-17 12:58:54 +0530

    base components

---
+++ src/components/CustomFilter.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface CustomFilterProps {}
+
+const CustomFilter: FC<CustomFilterProps> = (props) => {
+  return <div> CustomFilter</div>;
+};
+
+export default CustomFilter;
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface SearchBarProps {}
+
+const SearchBar: FC<SearchBarProps> = (props) => {
+  return <div> SearchBar</div>;
+};
+
+export default SearchBar;
