7454254aecd4ff91790a2f165a8a3d481643c17b
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-17 14:37:24 +0530

    search feature added

--- src/components/SearchManufacturer.tsx
+++ src/components/SearchManufacturer.tsx
@@ -1,6 +1,10 @@
-import { FC, useState } from "react";
+"use client";
+
+import { FC, useState, Fragment } from "react";
 import { Combobox, Transition } from "@headlessui/react";
 import Image from "next/image";
+
+import { manufacturers } from "@/constants";
 
 interface SearchManufacturerProps {
   manufacturer: string;
@@ -12,9 +16,19 @@ const SearchManufacturer: FC<SearchManufacturerProps> = (props) => {
 
   const [query, setQuery] = useState("");
 
+  const filteredManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, "")),
+        );
+
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]">
             <Image
@@ -34,6 +48,62 @@ const SearchManufacturer: FC<SearchManufacturerProps> = (props) => {
             placeholder="Volkswagen..."
           />
         </div>
+
+        {/* Transition for displaying the options */}
+        <Transition
+          as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          afterLeave={() => setQuery("")} // Reset the search query after the transition completes
+        >
+          <Combobox.Options
+            className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+            static
+          >
+            {filteredManufacturers.length === 0 && query !== "" ? (
+              <Combobox.Option
+                value={query}
+                className="search-manufacturer__option"
+              >
+                Create "{query}"
+              </Combobox.Option>
+            ) : (
+              filteredManufacturers.map((item) => (
+                <Combobox.Option
+                  key={item}
+                  className={({ active }) =>
+                    `relative search-manufacturer__option ${
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
+                    }`
+                  }
+                  value={item}
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {item}
+                      </span>
+
+                      {/* Show an active blue background color if the option is selected */}
+                      {selected ? (
+                        <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? "text-white" : "text-pribg-primary-purple"
+                          }`}
+                        ></span>
+                      ) : null}
+                    </>
+                  )}
+                </Combobox.Option>
+              ))
+            )}
+          </Combobox.Options>
+        </Transition>
       </Combobox>
     </div>
   );
