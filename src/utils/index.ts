bc158b8db424627efec5f481857e870131ff4634
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-17 17:07:47 +0530

    basic api

---
+++ src/utils/index.ts
@@ -0,0 +1,22 @@
+interface FilterProps {}
+
+export async function fetchCars(filters: FilterProps) {
+  // Set the required headers for the API request
+  const headers: HeadersInit = {
+    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  // Set the required headers for the API request
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla`,
+    {
+      headers: headers,
+    },
+  );
+
+  // Parse the response as JSON
+  const result = await response.json();
+
+  return result;
+}
