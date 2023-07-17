54732a342940905f3263fb81efd7bc97a4e4b2c9
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-17 12:03:28 +0530

    basic header nad footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface FooterProps {}
+
+const Footer: FC<FooterProps> = (props) => {
+  return <div> Footer</div>;
+};
+
+export default Footer;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface NavbarProps {}
+
+const Navbar: FC<NavbarProps> = (props) => {
+  return <div> Navbar</div>;
+};
+
+export default Navbar;
