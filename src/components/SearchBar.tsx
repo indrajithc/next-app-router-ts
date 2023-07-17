033c92ce8d18f149d54263b7b0906b2bb366ef84
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-17 17:56:28 +0530

    search btton

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,34 @@
 "use client";
 
 import { FC, useState } from "react";
+import Image from "next/image";
 
 import SearchManufacturer from "@components/SearchManufacturer";
+
+interface SearchButtonProps {
+  otherClasses: string;
+}
+
+const SearchButton: FC<SearchButtonProps> = (props) => {
+  const { otherClasses } = props;
+  return (
+    <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+      <Image
+        src={"/magnifying-glass.svg"}
+        alt={"magnifying glass"}
+        width={40}
+        height={40}
+        className="object-contain"
+      />
+    </button>
+  );
+};
 
 interface SearchBarProps {}
 
 const SearchBar: FC<SearchBarProps> = (props) => {
   const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
 
   const handleSearch = () => {};
 
@@ -18,6 +39,25 @@ const SearchBar: FC<SearchBarProps> = (props) => {
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+        <div className="searchbar__item">
+          <Image
+            src="/model-icon.png"
+            width={25}
+            height={25}
+            className="absolute w-[20px] h-[20px] ml-4"
+            alt="car model"
+          />
+          <input
+            type="text"
+            name="model"
+            value={model}
+            onChange={(e) => setModel(e.target.value)}
+            placeholder="Tiguan..."
+            className="searchbar__input"
+          />
+          <SearchButton otherClasses="sm:hidden" />
+        </div>
+        <SearchButton otherClasses="max-sm:hidden" />
       </div>
     </form>
   );
