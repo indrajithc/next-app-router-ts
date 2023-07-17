0aefbebc618dcd201ca9c0623ac86b822d178d46
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-17 12:50:02 +0530

    ui sync

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero: FC<HeroProps> = (props) => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, book, or rent a car - quickly and easily!
+          Find, book, rent a car—quick and super easy!
         </h1>
         <p className="hero__subtitle">
           Streamline your car rental experience with our effortless booking
@@ -28,8 +28,8 @@ const Hero: FC<HeroProps> = (props) => {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
-          <div className="hero__image-overlay"></div>
         </div>
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
