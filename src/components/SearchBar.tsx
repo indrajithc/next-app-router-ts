1c3ccace39b6e8e70a2b7dc92a08209a035f95f8
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-17 17:59:08 +0530

    search logic

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,6 +2,7 @@
 
 import { FC, useState } from "react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 import SearchManufacturer from "@components/SearchManufacturer";
 
@@ -30,7 +31,43 @@ const SearchBar: FC<SearchBarProps> = (props) => {
   const [manufacturer, setManufacturer] = useState("");
   const [model, setModel] = useState("");
 
-  const handleSearch = () => {};
+  const router = useRouter();
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    // Create a new URLSearchParams object using the current URL search parameters
+    const searchParams = new URLSearchParams(window.location.search);
+
+    // Update or delete the 'model' search parameter based on the 'model' value
+    if (model) {
+      searchParams.set("model", model);
+    } else {
+      searchParams.delete("model");
+    }
+
+    // Update or delete the 'manufacturer' search parameter based on the 'manufacturer' value
+    if (manufacturer) {
+      searchParams.set("manufacturer", manufacturer);
+    } else {
+      searchParams.delete("manufacturer");
+    }
+
+    // Generate the new pathname with the updated search parameters
+    const newPathname = `${
+      window.location.pathname
+    }?${searchParams.toString()}`;
+
+    router.push(newPathname);
+  };
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (manufacturer.trim() === "" && model.trim() === "") {
+      return alert("Please provide some input");
+    }
+
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  };
 
   return (
     <form onSubmit={handleSearch} className="searchbar">
