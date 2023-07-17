b8836cee40c7f49e8e1bb1647e3bbfeb65098f9d
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-17 15:15:11 +0530

    search bar added

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,26 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useState } from "react";
+
+import SearchManufacturer from "@components/SearchManufacturer";
 
 interface SearchBarProps {}
 
 const SearchBar: FC<SearchBarProps> = (props) => {
-  return <div> SearchBar</div>;
+  const [manufacturer, setManufacturer] = useState("");
+
+  const handleSearch = () => {};
+
+  return (
+    <form onSubmit={handleSearch} className="searchbar">
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
+    </form>
+  );
 };
 
 export default SearchBar;
