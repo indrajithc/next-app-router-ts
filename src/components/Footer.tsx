203ecc271981a7132d93fad60d25d3e231f33a14
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-17 12:44:29 +0530

    link mapped

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,47 @@
 import { FC } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import { footerLinks } from "@/constants";
 
 interface FooterProps {}
 
 const Footer: FC<FooterProps> = (props) => {
-  return <div> Footer</div>;
+  return (
+    <footer className="flex flex-col text-black-100  mt-5 border-t border-gray-100">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image
+            src={"/logo.svg"}
+            alt="loo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+          <p className="tex-base text-green-700">
+            Carhub 2023 <br />
+            All rights reserved &copy;
+          </p>
+        </div>
+        <div className="footer__links">
+          {footerLinks.map((link) => (
+            <div key={link.title} className="footer__link">
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item) => (
+                <Link
+                  key={item.title}
+                  href={item.url}
+                  className="text-gray-500"
+                >
+                  {item.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
