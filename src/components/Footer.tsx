e0828a55b137eb0aabce0db9cb55ed47f514f96f
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-17 12:45:58 +0530

    footer component updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,31 +13,45 @@ const Footer: FC<FooterProps> = (props) => {
         <div className="flex flex-col justify-start items-start gap-6">
           <Image
             src={"/logo.svg"}
-            alt="loo"
+            alt="logo"
             width={118}
             height={18}
             className="object-contain"
           />
-          <p className="tex-base text-green-700">
+          <p className="text-base text-gray-700">
             Carhub 2023 <br />
-            All rights reserved &copy;
+            All Rights Reserved &copy;
           </p>
         </div>
         <div className="footer__links">
-          {footerLinks.map((link) => (
-            <div key={link.title} className="footer__link">
-              <h3 className="font-bold">{link.title}</h3>
-              {link.links.map((item) => (
-                <Link
-                  key={item.title}
-                  href={item.url}
-                  className="text-gray-500"
-                >
-                  {item.title}
-                </Link>
-              ))}
+          {footerLinks.map((item) => (
+            <div key={item.title} className="footer__link">
+              <h3 className="font-bold">{item.title}</h3>
+              <div className="flex flex-col gap-5">
+                {item.links.map((link) => (
+                  <Link
+                    key={link.title}
+                    href={link.url}
+                    className="text-gray-500"
+                  >
+                    {link.title}
+                  </Link>
+                ))}
+              </div>
             </div>
           ))}
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+        <p>@2023 CarHub. All rights reserved</p>
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy & Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms & Condition
+          </Link>
         </div>
       </div>
     </footer>
