394aaeaa79f1f9b6dfc1f807877a0d275be6281f
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-17 18:02:45 +0530

    filter added

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,14 @@
-interface FilterProps {}
+interface FilterProps {
+  manufacturer?: string;
+  year?: number;
+  model?: string;
+  limit?: number;
+  fuel?: string;
+}
 
 export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
+
   // Set the required headers for the API request
   const headers: HeadersInit = {
     "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
@@ -9,7 +17,7 @@ export async function fetchCars(filters: FilterProps) {
 
   // Set the required headers for the API request
   const response = await fetch(
-    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla`,
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     {
       headers: headers,
     },
